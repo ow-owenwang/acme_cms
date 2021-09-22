03b73fec33bfd187560697e281bd40f28761f42c
Owen Wang <ow.owenwang@gmail.com>
2021-09-22 16:08:00 +0800

    项目初始化

---
+++ src/main/java/com/example/acme_cms/AcmeCmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.acme_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcmeCmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcmeCmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/acme_cms/AcmeCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.acme_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeCmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
